863976f47a8d2dd3f32b52d2dc1938e71c6af48b
PatrickkikALT <45902985+PatrickkikALT@users.noreply.github.com>
2024-07-14 21:54:00 +0200

    meerdere controller support player movement
    limiet van 4 mensen

---
+++ project party/Assets/C# Script/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.iOS.LowLevel;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Vector2 input;
+    public float jump;
+    public Rigidbody rb;
+    public bool grounded;
+    private bool isCoroutineRunning;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        transform.Translate(new Vector3(input.x, 0, input.y) * speed * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx) => input = ctx.ReadValue<Vector2>();
+
+    public void OnJump()
+    {
+        if (grounded)
+        {
+            rb.AddForce(0, jump, 0);
+            grounded = false;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        grounded = true;
+        //if (collision.collider.CompareTag("Ground"))
+        //{
+            
+        //}
+    }
+}
